299b19b43d01584fc36da14c6d642ab8078d8848
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-05-23 15:21:00 -0400

    Removed unneccessary junk from dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,9 @@
-import { requireAuth } from '@/utils/requireAuth';
-import { redirect } from 'next/navigation';
-
 const Dashboard = async () => {
-  try {
-    //await requireAuth();
-    return (
-      <div>
-        <h1>Dashboard</h1>
-      </div>
-    );
-  } catch (error) {
-    redirect('/login');
-  }
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
 };
 
 export default Dashboard;
