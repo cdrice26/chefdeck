014a9ed3dc603db3c8003d10d098f85fc11b731c
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-06-07 14:23:59 -0400

    Fixed home redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import { requireAuth } from '@/utils/requireAuth';
+import createClient from '@/utils/supabase/supabase';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  try {
-    const user = await requireAuth();
-    if (user) {
-      return redirect('/dashboard');
-    }
-  } catch (e) {}
-
-  return <div>Cooky</div>;
+  const supabase = await createClient();
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+  if (user) {
+    redirect('/dashboard');
+  } else {
+    return <div>Cooky</div>;
+  }
 }
