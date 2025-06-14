03617c0701bf0ab72c870b060567f7f2cd648e72
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-06-14 16:29:02 -0400

    Added nav buttons to tab index

--- src/components/navbar/Tab.tsx
+++ src/components/navbar/Tab.tsx
@@ -8,6 +8,7 @@ interface TabProps {
 const Tab = ({ label, isActive, onClick, icon }: TabProps) => {
   return (
     <button
+      tabIndex={0}
       className={`px-4 py-2 text-sm font-lg flex flex-row items-center gap-2 text-nowrap ${
         isActive
           ? 'text-blue-600 border-blue-600 bg-gray-200 dark:bg-[#333] rounded-full'
