0696f6e6e8b0d6d9aa402674126f2846be518a13
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-06-24 10:30:04 -0400

    fixed hover styles

--- src/components/navbar/Tab.tsx
+++ src/components/navbar/Tab.tsx
@@ -12,7 +12,7 @@ const Tab = ({ label, isActive, onClick, icon }: TabProps) => {
       className={`px-4 py-2 text-sm font-lg flex flex-row items-center gap-2 text-nowrap ${
         isActive
           ? 'text-blue-600 border-blue-600 bg-white/50 dark:bg-[#333]/50 rounded-full'
-          : 'text-gray-500 hover:bg-gray-200 dark:hover:bg-[#333] transition-colors duration-200 rounded-full'
+          : 'text-gray-500 hover:bg-gray-200/50 dark:hover:bg-[#333]/50 transition-colors duration-200 rounded-full'
       }`}
       onClick={onClick}
     >
