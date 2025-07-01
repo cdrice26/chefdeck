08d2764d7e8405b6cbb601efa9836536e2d37025
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-07-01 11:47:38 -0400

    Fixed recipe modals

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -20,7 +20,7 @@ const Modal = ({ children, className = '', usePortal = false }: ModalProps) =>
       document.body
     )
   ) : (
-    <div className='absolute inset-0 flex items-center justify-center backdrop-blur-md'>
+    <div className='relative h-full inset-0 flex items-center justify-center backdrop-blur-md'>
       <div className='absolute inset-0 bg-black opacity-50' />
       <div
         className={`p-4 pb-20 sm:pb-4 rounded shadow-lg flex flex-col gap-2 ${className} relative overflow-y-auto`}
