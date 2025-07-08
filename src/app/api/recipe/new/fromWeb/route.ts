4abfa8bbc2f40d180925a91f5312cc206d033027
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-07-08 11:20:56 -0400

    Added error handler to web request

--- src/app/api/recipe/new/fromWeb/route.ts
+++ src/app/api/recipe/new/fromWeb/route.ts
@@ -14,59 +14,68 @@ export const POST = async (req: NextRequest) => {
       { status: 400 }
     );
   }
-  const resp = await fetch(
-    `${process.env.PYTHON_API_URL}/scrape-recipe?url=${encodeURIComponent(url)}`
-  );
-  if (!resp.ok) {
-    if (resp.status === 429) {
-      return NextResponse.json(
-        {
-          error: { message: 'Too many requests. Try again in a minute.' }
-        },
-        { status: 429 }
-      );
-    }
-    if (resp.status === 403) {
+  try {
+    const resp = await fetch(
+      `${process.env.PYTHON_API_URL}/scrape-recipe?url=${encodeURIComponent(
+        url
+      )}`
+    );
+    if (!resp.ok) {
+      if (resp.status === 429) {
+        return NextResponse.json(
+          {
+            error: { message: 'Too many requests. Try again in a minute.' }
+          },
+          { status: 429 }
+        );
+      }
+      if (resp.status === 403) {
+        return NextResponse.json({
+          error: {
+            message: "That website doesn't permit Cooky to access its recipes."
+          }
+        });
+      }
       return NextResponse.json({
-        error: {
-          message: "That website doesn't permit Cooky to access its recipes."
-        }
+        error: { message: 'Error fetching recipe from website, check the URL.' }
       });
     }
-    return NextResponse.json({
-      error: { message: 'Error fetching recipe from website, check the URL.' }
-    });
-  }
-  const recipe = await resp.json();
-  const title = recipe.title;
-  const ingredients = recipe.ingredients.map(
-    (ing: Ingredient, idx: number) => ({
-      ...ing,
-      sequence: idx + 1
-    })
-  );
-  const yieldValue = recipe.servings;
-  const time = recipe.minutes;
-  const directions = recipe.directions;
-  const sourceUrl = recipe.source_url;
-
-  try {
-    const updateData = await createOrUpdateRecipe(
-      await getAccessToken(req),
-      title,
-      ingredients,
-      yieldValue,
-      time,
-      null,
-      directions,
-      [],
-      'white',
-      null,
-      sourceUrl
+    const recipe = await resp.json();
+    const title = recipe.title;
+    const ingredients = recipe.ingredients.map(
+      (ing: Ingredient, idx: number) => ({
+        ...ing,
+        sequence: idx + 1
+      })
     );
-    return NextResponse.json(updateData, { status: 200 });
+    const yieldValue = recipe.servings;
+    const time = recipe.minutes;
+    const directions = recipe.directions;
+    const sourceUrl = recipe.source_url;
+
+    try {
+      const updateData = await createOrUpdateRecipe(
+        await getAccessToken(req),
+        title,
+        ingredients,
+        yieldValue,
+        time,
+        null,
+        directions,
+        [],
+        'white',
+        null,
+        sourceUrl
+      );
+      return NextResponse.json(updateData, { status: 200 });
+    } catch (err) {
+      console.log(err);
+      return getErrorResponse(err as PostgrestError);
+    }
   } catch (err) {
-    console.log(err);
-    return getErrorResponse(err as PostgrestError);
+    return NextResponse.json(
+      { error: { message: 'Failed to fetch recipe' } },
+      { status: 500 }
+    );
   }
 };
