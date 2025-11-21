55cbb247ba13ff697e4081bee19d69e9c9e1d991
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-11-21 15:42:47 -0500

    Migrated forgot password page to new architecture

--- src/app/forgotPassword/page.tsx
+++ src/app/forgotPassword/page.tsx
@@ -1,49 +1,12 @@
 'use client';
 
-import Button from '@/components/forms/Button';
-import Input from '@/components/forms/Input';
-import ResponsiveForm from '@/components/forms/ResponsiveForm';
-import request from '@/utils/fetchUtils';
-import { useState } from 'react';
+import ForgotPassword from '@/components/pages/ForgotPassword';
+import usePasswordMutator from '@/hooks/usePasswordMutator';
 
-const ForgotPassword = () => {
-  const [error, setError] = useState<string | null>(null);
-  const [message, setMessage] = useState<string | null>(null);
+const ForgotPasswordPage = () => {
+  const mutator = usePasswordMutator();
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setError(null);
-
-    const formData = new FormData(e.currentTarget);
-    const email = formData.get('email');
-
-    const res = await request(
-      '/api/auth/forgotPassword',
-      'POST',
-      JSON.stringify({ email })
-    );
-
-    if (!res.ok) {
-      const data = await res.json();
-      setError(data.error || 'Password reset failed');
-      return;
-    }
-
-    setMessage(
-      'Password reset email sent successfully. Please check your inbox.'
-    );
-  };
-
-  return (
-    <ResponsiveForm onSubmit={handleSubmit}>
-      <h1>Forgot Password</h1>
-      <Input type='email' name='email' placeholder='Email' required />
-
-      <Button type='submit'>Reset Password</Button>
-      {error && <div style={{ color: 'red' }}>{error}</div>}
-      {message && <div style={{ color: 'green' }}>{message}</div>}
-    </ResponsiveForm>
-  );
+  return <ForgotPassword {...mutator} />;
 };
 
-export default ForgotPassword;
+export default ForgotPasswordPage;
