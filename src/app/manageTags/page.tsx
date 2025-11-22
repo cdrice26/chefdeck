bd68f9e3eed427341598a2af8a6f8c4ec4d7d60b
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-11-21 20:34:23 -0500

    Removed unused imports

--- src/app/manageTags/page.tsx
+++ src/app/manageTags/page.tsx
@@ -1,11 +1,7 @@
 'use client';
 
-import Button from '@/components/forms/Button';
-import ResponsiveForm from '@/components/forms/ResponsiveForm';
 import { useNotification } from '@/context/NotificationContext';
 import useAvailableTags from '@/hooks/fetchers/useAvailableTags';
-import { OptionType } from '@/components/forms/TagSelector';
-import request from '@/utils/fetchUtils';
 import useTagsMutator from '@/hooks/mutators/useTagsMutator';
 import ManageTags from '@/components/pages/ManageTags';
 
