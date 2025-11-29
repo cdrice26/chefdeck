3f1f50897f366751b36a52a8b5028e567c4b1dfc
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-11-29 09:03:47 -0500

    Added window vibrancy

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -1,4 +1,8 @@
 use tauri::{LogicalPosition, TitleBarStyle, WebviewUrl, WebviewWindowBuilder};
+#[cfg(target_os = "windows")]
+use window_vibrancy::apply_blur;
+#[cfg(target_os = "macos")]
+use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -6,6 +10,7 @@ pub fn run() {
         .setup(|app| {
             let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
                 .title("ChefDeck")
+                .transparent(true)
                 .inner_size(800.0, 600.0);
 
             // set transparent title bar only when building for macOS
@@ -15,7 +20,24 @@ pub fn run() {
                 .title_bar_style(TitleBarStyle::Overlay)
                 .traffic_light_position(LogicalPosition::new(18.0, 25.0));
 
-            let _window = win_builder.build().unwrap();
+            let window = win_builder.build().unwrap();
+
+            #[cfg(target_os = "macos")]
+            {
+                // macOS vibrancy
+                apply_vibrancy(&window, NSVisualEffectMaterial::Sidebar, None, None)
+                    .expect("Unsupported platform or failed to apply vibrancy");
+            }
+
+            #[cfg(target_os = "windows")]
+            {
+                // Windows blur or acrylic
+                // Choose one:
+                apply_blur(&window, Some((18, 18, 18, 125))).expect("Failed to apply blur");
+                // or
+                // apply_acrylic(&window, Some((0, 0, 0, 125)))
+                //     .expect("Failed to apply acrylic");
+            }
 
             if cfg!(debug_assertions) {
                 app.handle().plugin(
--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <div data-tauri-drag-region style={{ backgroundColor: "transparent", width: "100%", height: "60px" }}>
+        Move me!
+    </div>
   </React.StrictMode>,
 )
