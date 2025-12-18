64f38563a06affe80b687f6a863a33a7cc347539
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2025-12-18 08:59:05 -0500

    Fixed TS Errors

--- packages/shared/src/types/RequestFn.ts
+++ packages/shared/src/types/RequestFn.ts
@@ -1,7 +1,14 @@
+export interface IPCResponse {
+  json: () => Promise<object>;
+  text: () => Promise<string>;
+  ok: boolean;
+  status: number;
+}
+
 type RequestFn = (
   url: string,
   method: string,
   body?: FormData | object
-) => Promise<Response>;
+) => Promise<IPCResponse | Response>;
 
 export default RequestFn;
