0493363faa31a76651ec6571bc1d3dbc5c02aadb
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2026-06-16 21:03:18 -0400

    fix: Removed console.log

--- apps/desktop/src/utils/fetchUtils.ts
+++ apps/desktop/src/utils/fetchUtils.ts
@@ -143,7 +143,6 @@ export const requestFromFormData: RequestFn = async (url, _method, body) => {
     throw new Error('Body must be a FormData object');
   } else {
     const recipeObj = getRecipeUpdateData(body);
-    console.log(recipeObj);
     return await request(url, _method, recipeObj);
   }
 };
